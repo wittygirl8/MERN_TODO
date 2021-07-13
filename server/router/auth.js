91e428dfac18499d494b23990837718576455ab3
Riddhi <riddhisinghreena238@gmail.com>
2021-07-13 09:05:06 +0530

    user registration with async await

--- server/router/auth.js
+++ server/router/auth.js
@@ -9,7 +9,31 @@ router.get('/', (req,res)=>{
     res.send('Hello todo from the server router.js');
 });
 
-router.post('/register', (req,res)=>{
+//Using Promises
+// router.post('/register', (req,res)=>{
+//     const { name, email, phone, password, cpassword } = req.body;
+    
+//     if( !name || !email || !phone || !password || !cpassword ){
+//         return res.status(422).json({error:"Field is Required"})
+//         }
+
+//     //Registration of a user in mongoDB atles
+//     User.findOne({ email: email })
+//     .then((userExist) =>{
+//         if(userExist)
+//             return res.status(422).json({error:"Email already exists."});
+
+//         const user = new User({name, email, phone, password, cpassword})
+        
+//         user.save().then(() =>{
+//             res.status(201).json({message: "user registered successfully."})
+//         }).catch((err) => res.status(500).json({ error: "Failed to registed"}));
+    
+//     }).catch(err =>{console.log(err); });
+
+
+//Using async await
+router.post('/register', async (req,res)=>{
     const { name, email, phone, password, cpassword } = req.body;
     
     if( !name || !email || !phone || !password || !cpassword ){
@@ -17,18 +41,54 @@ router.post('/register', (req,res)=>{
         }
 
     //Registration of a user in mongoDB atles
-    User.findOne({ email: email })
-    .then((userExist) =>{
-        if(userExist)
-            return res.status(422).json({error:"Email already exists."});
 
-        const user = new User({name, email, phone, password, cpassword})
-        
-        user.save().then(() =>{
-            res.status(201).json({message: "user registered successfully."})
-        }).catch((err) => res.status(500).json({ error: "Failed to registed"}));
+    try{
+        const userExist = await User.findOne({ email: email })
+
+        if(userExist)
+                return res.status(422).json({error:"Email already exists."});
     
-    }).catch(err =>{console.log(err); });
+        const user = new User({name, email, phone, password, cpassword})
+            
+        const userRegister = await user.save();
+
+        if(userRegister){
+            res.status(201).json({message: "user registered successfully."})
+        }
+        else{
+            res.status(500).json({ error: "Failed to registed"})
+        }
+    }
+    catch(err){
+        console.log(err);
+    }
 });
+
+
+//login route
+
+router.post('/signin', async (req, res) => {
+    // console.log(req.body);
+    // res.json({message:"login awsome"});
+
+    try{
+        const { email, password } = req.body;
+        if( !email || !password ){
+            return res.status(400).json({error:"Required"})
+        }
+
+        const userLogin = await User.findOne({ email: email })
+        if( !userLogin ){
+            res.status(400).json({error:"Invalid Credential"})
+           
+        }
+        else{
+            res.json({message:"User SignIn successfully"});
+        }
+    }
+    catch(err){
+        console.log(err);
+    }
+})
 
 module.exports=router;
