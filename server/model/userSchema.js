49eb6da07e6870d3db4d89b415a23da0f7dd312a
Riddhi <riddhisinghreena238@gmail.com>
2021-07-13 19:43:58 +0530

    user registaration with async and await login with jsonwebtoken

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -6,6 +7,28 @@ const userSchema = new mongoose.Schema({
     phone: { type: Number, required: true },
     password: { type: String, required: true },
     cpassword: { type: String, required: true },
+    todo: { type: Array },
+    tokens:[
+      {
+        token:{ type: String, required: true}
+      }
+    ]
   });
-  
+
+  //Token generation
+
+  userSchema.methods.generateAuthToken = async function(){
+    try{
+      let token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+      this.tokens = this.tokens.concat({ token:token });
+      await this.save();
+      return token;
+    } 
+    catch(err){
+      console.log(err);
+    }
+  }
+
+
   module.exports = mongoose.model('USER', userSchema);
+
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 //Database Connection
 require('../db/conn');
 const User = require('../model/userSchema');
+
 
 router.get('/', (req,res)=>{
     res.send('Hello todo from the server router.js');
@@ -47,16 +49,20 @@ router.post('/register', async (req,res)=>{
 
         if(userExist)
                 return res.status(422).json({error:"Email already exists."});
-    
-        const user = new User({name, email, phone, password, cpassword})
-            
-        const userRegister = await user.save();
-
-        if(userRegister){
-            res.status(201).json({message: "user registered successfully."})
-        }
+        else if(password != cpassword)
+            return res.status(422).json({error:"Password doesn't match"});
         else{
-            res.status(500).json({ error: "Failed to registed"})
+            const user = new User({name, email, phone, password, cpassword})
+            
+            //Password hasing Middelware: code is in userScema.js
+        
+            const userRegister = await user.save();
+            if(userRegister){
+                res.status(201).json({message: "user registered successfully."})
+            }
+            else{
+                res.status(500).json({ error: "Failed to registed"})
+            }
         }
     }
     catch(err){
@@ -70,20 +76,34 @@ router.post('/register', async (req,res)=>{
 router.post('/signin', async (req, res) => {
     // console.log(req.body);
     // res.json({message:"login awsome"});
-
+    let token;
     try{
         const { email, password } = req.body;
         if( !email || !password ){
             return res.status(400).json({error:"Required"})
         }
 
-        const userLogin = await User.findOne({ email: email })
-        if( !userLogin ){
-            res.status(400).json({error:"Invalid Credential"})
-           
+        const userLogin = await User.findOne({ email: email });
+
+        if(userLogin){
+            
+            token = await userLogin.generateAuthToken();
+            console.log(token);
+
+            res.cookie("jwtoken", token, {
+                expires: new Date(Date.now() + 25892000000),
+                httpOnly: true
+            });
+            if(password != userLogin.password){
+                res.status(400).json({error:"Invalid Credential"})
+               
+            }
+            else{
+                res.json({message:"User SignIn successfully"});
+            }
         }
         else{
-            res.json({message:"User SignIn successfully"});
+            res.status(400).json({error:"Invalid Credential"})
         }
     }
     catch(err){
